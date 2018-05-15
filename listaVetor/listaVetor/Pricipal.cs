a32ea1de34af53eceb8d67ee4da8e29827ebb580
andreicampigotto <andreicampigotto@gmail.com>
2018-05-15 17:14:06 -0300

    lista começada
    1 feita

---
+++ listaVetor/listaVetor/Pricipal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaVetor
+{
+    class Pricipal
+    {
+        static void Main(string[] args)
+        {
+            new Exercicio01();
+        }
+    }
+}
